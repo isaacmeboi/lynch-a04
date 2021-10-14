e8ef40a170add96be8f6ab354cb0ac55d4535e99
isaac lynch <thebunnyarcher@gmail.com>
2021-10-14 14:32:04 -0400

    skele code

---
+++ exercise42/src/main/java/baseline/Solution24.java
@@ -0,0 +1,8 @@
+package baseline;
+
+public class Solution24 {
+    //make a method to print the details in tabular format
+//use a while loop to read in all the names until there aren't anymore, line by line
+//ensure to have a catch so that it gives a warning if the file isnt found
+//parse the strng using regex
+}
