5f2996a019813b82342e7325995ba64c162ef97a
isaac lynch <thebunnyarcher@gmail.com>
2021-10-14 15:50:13 -0400

    skele code

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class Solution43 {
+    //make a function to create a web folder
+//set the directory and create a file w/ that path
+//create folder
+//make function for the JS folder as well
+//do the same thing done with web folder
+//make a function to create CSS folder
+//repeat same previous steps again
+//make a function to create HTML
+//create and write on the file
+//make sure to do a catch that returns some fail msg if anything goes wrong
+//obv read in inputs from user for the site name, author, and if they want CSS/JS folders
+//then create the html, website, and optional JS and CSS folders
+}
