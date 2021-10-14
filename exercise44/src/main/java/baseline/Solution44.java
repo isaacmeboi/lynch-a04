a889faad32a8fe980f4a2a3520c114dd9ed37628
isaac lynch <thebunnyarcher@gmail.com>
2021-10-14 15:57:01 -0400

    skele code

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class Solution44 {
+    //create JSON parder
+//prob read the json file using the parser? and store it?
+//make an input string and have a flag to determine whether it exists or not
+//create iterator to loop through
+//get the value of the name in the loop and compare it with the input
+//have so if the falg sets off as zero that it prints that the item wasnt found
+}
