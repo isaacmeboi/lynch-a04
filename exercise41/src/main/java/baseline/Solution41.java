15639aad0c73e58ec7650e0d3e4c0f96a9fce26c
isaac lynch <thebunnyarcher@gmail.com>
2021-10-13 22:00:51 -0400

    skele code

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,17 @@
+package baseline;
+
+public class Solution41 {
+    //define function to red data from scanner to arraylist
+//have it so that the data is read until the file has nextline
+//define function to print names in file
+//make a filewriter for output file
+//make sure to print the correct formatting
+//have the names printed under the header
+//close the filewriter to avoid mem. leakage
+//make scanner to read input
+//make arraylist to store names
+//read names from input file
+//sort names
+//close scanner after reading file
+
+}
