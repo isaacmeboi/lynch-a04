cdf11f1ea23eb997d56bbaa9c0d717ab569ebf6e
isaac lynch <thebunnyarcher@gmail.com>
2021-10-15 19:28:05 -0400

    skele code

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class Solution46 {
+    //make a class for reading input file
+//have the the input split by spaces using a loop for
+//see if it's in the map already
+//return map at the end
+
+//make a class to print the report as well
+//use arrays to store the values and keys for the whole map loop thing
+//have the value array's sort reversed
+//sort the keys array accorsing to val. array and store in some new temp. key
+//then print the histogram
+
+//have the main function print the report to the screen for user to see
+}
