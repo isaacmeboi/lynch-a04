9c2e44e9a86b2bb9555ca56229d502c5a0963475
isaac lynch <thebunnyarcher@gmail.com>
2021-10-15 17:08:01 -0400

    skele code

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class Solution45 {
+    //first we'll have to get the file read, probably using a readfIle class
+//the readfile class will need to read in the file line by line
+//then the file is then stored into a list
+//we'll have to return the list at the end
+
+//then we'll have to write the file using the writefile object
+//probably gonna have to make a loop to loop the size of the list
+//also count mods here for the console output
+//and then write the file
+
+//the # of modifications will have to get printed out to the console
+
+}
